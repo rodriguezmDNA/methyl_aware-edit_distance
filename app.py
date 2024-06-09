39b974617400e2e292585ecb9862b492f00abee3
rodriguezmDNA <rodriguezmDNA@users.noreply.github.com>
2024-06-08 19:34:48 -0700

    Add module and streamit app

---
+++ app.py
@@ -0,0 +1,164 @@
+### Streamlit
+from random import shuffle
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+from methyl_aware_distance import MethylAwareDistance
+
+
+import pandas as pd
+import numpy as np
+import altair as alt
+
+
+#### Styling functions
+stHTML = lambda string: st.markdown(string, unsafe_allow_html=True)
+stHTMLsidebar = lambda string: st.sidebar.markdown(string, unsafe_allow_html=True)
+textCenter = lambda text: f"<p style='text-align:center;'> {text} </p>"
+center_string = lambda x: f"<p style='text-align:center;'> {x} </p>"
+
+
+##########################################
+
+########################################## SIDEBAR #############################################
+#st.sidebar.title('Sidebar')
+st.sidebar.markdown('---')
+
+
+html_string = "<h1 style='text-align:center;'> An interactive tool for methyl-aware edit distance calculations </h1>"
+stHTML(html_string)
+st.markdown('---')
+
+
+
+
+expected_sequence = st.text_input("Expected sequence","ACTG")
+observed_sequence = st.text_input("Observed sequence","ATTG")
+
+
+
+# Boolean input (checkbox)
+is_read_1 = st.checkbox("Sequence from Read 1? (if read 2 select False)")
+
+
+if expected_sequence and observed_sequence:
+
+    edit_distance = MethylAwareDistance('ACTG','ATTGA',is_read_1)
+    
+    if edit_distance.equal_length:
+        st.write("Hamming distance", edit_distance.hamming_methyl_aware())
+    else:
+        st.write("Levenshtein distance", edit_distance.space_efficient_levenshtein_methyl_aware())
+
+
+
+# #st.sidebar.markdown("<h1 style='text-align:center;'> This title is HTML </h1>", unsafe_allow_html=True)
+
+# stHTMLsidebar("<h1 style='text-align:center;'> Distribution parameters </h1>")
+
+# ## Selector
+# st.sidebar.markdown('Select average weight :rabbit:  :point_down:')
+# mu = st.sidebar.slider('',1,6,value=3,step=1)
+
+# stHTMLsidebar('<br>')
+
+# sigma = st.sidebar.slider('+-:',0.0,3.0,value=0.5,step=0.5)
+
+
+# st.markdown('<br>', unsafe_allow_html=True)
+# stHTML('<br>')
+
+# stHTMLsidebar("<h1 style='text-align:center;'> Sample size </h1>")
+
+# st.sidebar.markdown('Select sample size of :rabbit2: weights :point_down:')
+# sample_size = st.sidebar.slider('sample size:',1,30,value=3,step=1)
+
+# st.sidebar.markdown('Number of random samples :point_down:')
+# n_samples = st.sidebar.slider('sample size:',1,100,value=5,step=1)
+
+# st.sidebar.markdown('---')
+
+# #############################################################################################
+
+
+# ########################################## MAIN #############################################
+
+# html_string = "<h1 style='text-align:center;'> Sampler with normal distribution </h1>"
+# stHTML(html_string)
+# st.markdown('---')
+
+
+
+# #############################################
+
+
+# def get_pdf(mu, sigma):
+#     # Mean and standard deviation
+
+#     # Generating points on the x axis between -4 and 4, which is typical for a normal distribution plot
+#     x = np.linspace(mu - 4*sigma, mu + 4*sigma, 1000)
+
+#     # Calculating the probability density function for the normal distribution
+#     pdf = (1 / (np.sqrt(2 * np.pi * sigma**2))) * np.exp(-0.5 * ((x - mu) ** 2) / (sigma ** 2))
+#     return pd.DataFrame({'X':x,'PDF':pdf})
+
+
+# # Step 2: Function to extract a sample of n weights from the distribution
+# def sample_weights(mu, sigma, n):
+#     return np.random.normal(mu, sigma, n)
+
+# # Step 3: Function to plot the mean of the sample onto the previous plot
+# def get_sample_mean(mu, sigma, n):
+#     sample = sample_weights(mu, sigma, n)
+#     return np.mean(sample)
+
+
+# underlying_distribution = get_pdf(mu, sigma)
+
+
+# sample_means = [(get_sample_mean(mu,sigma, sample_size),.01) for i in range(n_samples)]
+# sample_means = pd.DataFrame(sample_means,columns=['mean','ypos'])
+
+
+# #Chart with tooltip 
+# st.markdown('<style>#vg-tooltip-element{z-index: 1000051}</style>', unsafe_allow_html=True)#hack to make tooltip visible in full-screen mode
+
+# chart = alt.Chart(underlying_distribution).mark_line().encode(
+#     x=alt.X('X',title=f'distribution of weight'),
+#     y=alt.Y('PDF',axis=None),
+#     color=alt.value('black')
+
+# ).properties(
+#     title=f'Normal Distribution of Rabbit Weights | Mean={mu} kg, SD={sigma} kg',
+#     width=600,
+#     height=300,
+    
+# )
+
+
+
+# points_chart = alt.Chart(sample_means).mark_point(filled=True).encode(
+#     x='mean',
+#     y='ypos',
+#     color=alt.value('red')
+# )
+
+
+# chart = (chart + points_chart).interactive()
+# chart = chart.configure(background='white')
+# st.altair_chart(chart, use_container_width=True)
+
+
+
+# ### Some space
+# stHTML('<br>')
+
+
+#############################################
+stHTML('<br>')
+stHTML('<br>')
+st.markdown('---')
+st.write('Joel Rodriguez-Medina')
+#logoDir = 'https://1jj36121ulnl3yz5ue21q04x-wpengine.netdna-ssl.com/wp-content/uploads/2020/05/Foodome_Logo.png'
+#components.html(f"<img src='{logoDir}' style='width:25%;margin-left: auto;margin-right: auto;display: block'>")
---
+++ methyl_aware_distance.py
@@ -0,0 +1,177 @@
+from typing import Tuple
+import numpy as np 
+
+class MethylAwareDistance:
+
+    def __init__(self, expected: str, observed:str,
+                 read_1: bool = True,
+                 match: int = 0, mismatch: int = 1, indel: int =1):
+
+        self.expected = expected
+        self.observed = observed
+        self.read_1 = read_1
+
+        self.size_observedatch = match
+        self.size_observedismatch = mismatch
+        self.indel =  indel
+
+        self.expected = self.expected.upper()
+        self.observed = self.observed.upper()
+
+        self.size_expected = len(self.expected) 
+        self.size_observed = len(self.observed)
+        self.equal_length = self.size_expected == self.size_observed
+
+        self.equivalent_bases = self.base_equivalency()
+
+    def base_equivalency(self) -> Tuple[str,str]: 
+
+        """
+        Return equivalent bases during methyl converexpectedion (C>T or G>A) depending on the read (read 1 or read 2).
+
+        Methylated Cs are generally converted to T during conversion.
+        If conversion efficiency is low, methylated Cs can still be converted to T.
+        
+        
+        -----> Read 1
+        ================ Fragment ==============
+                                <----- Read 2                                
+
+
+        ## Read 1
+        5'---mCG---3'
+            
+        Bad protection: 5'---TG---3'
+        Good protection: 5'---CG---3'
+
+
+        ## Read 2
+    
+        5'---CpG---3'
+        3'---GmC---5'  || Read 2: bottom strand
+        
+
+        3'---GT---5'  || C>T in the bottom strand
+
+
+        5'---CA---3'  || Read as a G>A conversion when normalized to 5'>3'
+        3'---GT---5'  || 
+        """
+
+        return ('C','T') if self.read_1 else ('G','A')
+
+    def hamming_methyl_aware(self) -> int:
+        """
+        Calculate the methylation-aware Hamming distance between two sequences.
+        
+        Due to the nature of cytosine conversion, the order of the sequences matters.
+        """
+        
+        if not self.equal_length:
+            raise ValueError("Error: lengths of sequences should be the same.")
+
+        else:
+            distance = 0
+            
+            for base_exp, base_obs in zip(self.expected, self.observed):
+                if (base_exp != base_obs) and not (base_exp, base_obs) == self.equivalent_bases:
+                    distance += self.size_observedismatch
+            
+            return distance
+
+
+    def methyl_aware_base_score(self, s_i: str, t_j: str) -> int: 
+        '''
+        Return the score for a match or indel/mismatch accounting for methyl conversion
+        '''
+        return self.size_observedatch if (s_i == t_j) or (s_i, t_j) == self.equivalent_bases else self.size_observedismatch
+
+    
+    def levenshtein_methyl_aware(self) -> int:
+
+
+        distance_matrix = np.zeros((self.size_expected+1,
+                       self.size_observed+1),
+                       dtype=int)
+
+        distance_matrix[0,1:] = self.indel
+        distance_matrix[0,:] = np.cumsum(distance_matrix[0,:])
+        
+        distance_matrix[1:,0] = self.indel
+        distance_matrix[:,0] = np.cumsum(distance_matrix[:,0])
+        
+        for i in range(1, self.size_expected + 1):
+            for j in range(1, self.size_observed + 1):
+
+                match_mismatch_score = self.methyl_aware_base_score(self.expected[i - 1],
+                                                                    self.observed[j - 1]
+                                                                    )
+                
+
+                # if (expected[i - 1] == observed[j - 1]) or (expected[i - 1],observed[j - 1]) == equivalent_bases:
+                #     dp[i][j] = dp[i - 1][j - 1] + match
+                # else:    
+                indelmm = [
+                        (distance_matrix[i - 1][j] + self.indel), # indel in the expected sequence
+                        (distance_matrix[i][j - 1] + self.indel), # indel in the observed sequence
+                        (distance_matrix[i - 1][j - 1] + match_mismatch_score) # mismatch in the 
+                        ]
+                
+                distance_matrix[i][j]  = indelmm[np.argmin(indelmm)] 
+
+        return int(distance_matrix[-1,-1]) 
+
+
+    def fill_distance_matrix(self, 
+                             distance_matrix: np.ndarray,
+                             index_observed: int
+                             ):
+        """
+        Helper function for methyl-aware and space-efficient Levenshtein distance calculation.
+        Takes in a 2-column matrix and updates the distance between the observed and expected sequences at a j point.
+        Output is the updated matrix.
+        """
+        
+        for index_expected in range(1, self.size_expected+1):
+
+            ## Calculate match or mismatch score based on current characters from expected and observed sequences
+            match_mismatch_score = self.methyl_aware_base_score( self.expected[index_expected - 1],
+                                                                             self.observed[index_observed - 1]
+                                                                             )
+            
+
+            # Calculate possible scores from different operations:
+            scores = [
+                distance_matrix[index_expected, 0] + self.indel,    # score from insertion/deletion in expected
+                distance_matrix[index_expected-1, 1] + self.indel,  # score from insertion/deletion in observed
+                distance_matrix[index_expected-1, 0] + match_mismatch_score  # score from match/mismatch base in observed and expected
+            ]
+
+            # Select the score that minimizes the number of edits
+            distance_matrix[index_expected, 1] = min(scores)
+
+        return distance_matrix
+
+    def space_efficient_levenshtein_methyl_aware(self) -> int:
+
+        """
+        Function to calculate methyl-aware Levenshtein distance using a space-efficient implementation.
+        For long sequences, filling a dynamic programming matrix is not efficient.
+
+        Due to the nature of cytosine conversion, the order of the sequences matters.
+        """
+
+
+        # Initialize dynamic programming matrix. Two columns for space efficiency
+        
+        distance_matrix = np.zeros(( self.size_expected + 1, 2 ))
+        distance_matrix[:,0]  = np.array( [0] + [i * self.indel for i in range(1, self.size_expected+1) ])
+        distance_matrix[:,0] = distance_matrix[:,0].reshape(self.size_expected+1)
+        
+        # Iterate over elements of the second sequence
+        for index_observed in range(1, self.size_observed + 1):
+            distance_matrix[0, 1] = index_observed * self.indel
+            distance_matrix = self.fill_distance_matrix(distance_matrix, index_observed)
+            distance_matrix[:, 0] = distance_matrix[:, 1]
+        
+        return int(distance_matrix[-1,-1])
